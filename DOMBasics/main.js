ed0a54b612c40ec95adb497a0506a3793fbf74cb
Nabendu Biswas <nabendu.biswas@gmail.com>
2021-12-16 10:59:32 +0530

    JS Day-3

--- DOMBasics/main.js
+++ DOMBasics/main.js
@@ -10,10 +10,50 @@
 // console.log(document.querySelectorAll('.item'));
 // console.log(document.getElementsByClassName('item'));
 // console.log(document.getElementsByTagName('li'));
-const nodeItems = document.querySelectorAll('.item');
-const classItems = document.getElementsByClassName('item');
+// const nodeItems = document.querySelectorAll('.item');
+// const classItems = document.getElementsByClassName('item');
 // nodeItems.forEach(item => console.log(item));
 // classItems.forEach(item => console.log(item));
-for(itm of classItems){
-    console.log(itm);
+// for(itm of classItems){
+//     console.log(itm);
+// }
+
+//DOM Manipulation
+// const ul = document.querySelector('.items');
+
+//ul.remove();
+
+
+//Event Listener
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', e => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.backgroundColor = '#ccc';
+//     ul.firstElementChild.textContent = 'HTML';
+//     ul.children[1].innerText = 'CSS';
+//     ul.lastElementChild.innerHTML = '<h4>JavaScript</h4>';
+// })
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+    if(nameInput.value === "" || emailInput.value === "") {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(nameInput.value + ': ' + emailInput.value));
+        userList.appendChild(li);
+        nameInput.value = '';
+        emailInput.value = '';
+    }
 }
