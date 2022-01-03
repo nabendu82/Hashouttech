9e810ff4130efd110facc6c9a631696d83bde4f2
Nabendu Biswas <nabendu.biswas@gmail.com>
2022-01-03 11:29:33 +0530

    React Day-4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,13 @@
 import './App.css';
-import DemoForm from './components/DemoForm';
-// import StyleSheets from './components/StyleSheets';
-// import PersonList from './components/PersonList';
-// import ParentComponent from './components/ParentComponent';
-// import UserGreeting from './components/UserGreeting';
-// import EventBind from './components/EventBind';
-// import Counter from './components/Counter';
-//import JsxEx from './components/JsxEx';
-// import { Greet, Hello } from './components/Greet';
-// import Greeting from './components/Greeting';
-// import Welcome from './components/Welcome';
+import GrandParent from './components/GrandParent';
+import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      {/* <Greet />
-      <Hello />*/}
-      {/* <JsxEx /> */}
-      {/* <Greeting name="JavaScript">Language of the web</Greeting>
-      <Greeting name="Golang" />
-      <Greeting name="Swift">Language for ios app</Greeting>
-      <Welcome name="flutter">Language for cross-mobile development</Welcome> 
-      <Welcome name="React Native" /> 
-      <Welcome name="Java"><button>Click</button></Welcome>  */}
-      {/* <Counter /> */}
-      {/* <EventBind /> */}
-      {/* <ParentComponent /> */}
-      {/* <UserGreeting /> */}
-      {/* <PersonList /> */}
-      {/* <StyleSheets /> */}
-      <DemoForm />
+      <UserProvider value="Nabendu">
+        <GrandParent />
+      </UserProvider>
     </div>
   );
 }
