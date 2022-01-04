f6d12eb2fb8bef08e897069a6b176108d310c7a2
Nabendu Biswas <nabendu.biswas@gmail.com>
2022-01-04 11:04:01 +0530

    React Day-5

---
+++ hooks/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import HookCounter2 from './components/HookCounter2';
+import HookCounter4 from './components/HookCounter4';
+// import ClassCounter from './components/ClassCounter';
+// import HookCounter from './components/HookCounter';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter />
+      <HookCounter /> */}
+      {/* <HookCounter2 /> */}
+      <HookCounter4 />
+    </div>
+  );
+}
+
+export default App;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import GrandParent from './components/GrandParent';
-import { UserProvider } from './components/userContext';
+import PostForm from './components/PostForm';
+//import PostList from './components/PostList';
+// import GrandParent from './components/GrandParent';
+// import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      <UserProvider value="Nabendu">
+      {/* <UserProvider value="Nabendu">
         <GrandParent />
-      </UserProvider>
+      </UserProvider> */}
+      {/* <PostList /> */}
+      <PostForm />
     </div>
   );
 }
