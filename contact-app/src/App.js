dcb9282ca914cb8f088fa6701552be1dea34f3d9
Nabendu Biswas <nabendu.biswas@gmail.com>
2022-01-07 11:03:12 +0530

    React Day-7

---
+++ contact-app/src/App.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import AddContact from './components/AddContact';
+import ContactList from './components/ContactList';
+import Header from './components/Header';
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const removeContactHandler = id => {
+    const newContactList = contacts.filter(contact => contact.id !== id);
+    setContacts(newContactList);
+  }
+
+  const addContactHandler = contact => {
+    console.log(contact);
+    setContacts([...contacts, { id: Math.random(), ...contact }]);
+  }
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+    </div>
+  );
+}
+
+export default App;
