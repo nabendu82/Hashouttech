f89f7e5cdf2bd18aa863b047a4d797aba56932d7
Nabendu Biswas <nabendu.biswas@gmail.com>
2022-01-18 11:03:09 +0530

    Final Commit

--- tiktok-mern-hashout/frontend/src/App.js
+++ tiktok-mern-hashout/frontend/src/App.js
@@ -1,26 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Video from './components/Video';
+import axios from './components/axios';
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    async function fetchData(){
+      const res = await axios.get("/v2/posts")
+      setVideos(res.data)
+      return res
+    }
+    fetchData()
+  },[])
+
+  console.log(videos)
+
   return (
     <div className="app">
       <div className="app__videos">
-        <Video 
-          url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169739/video2_mecbdo.mp4"
-          channel="thewebdev"
-          description="This is a Windows PC" 
-          song="I am windows PC"
-          likes={145} 
-          shares={100} 
-          messages={45} />
-        <Video 
-          url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
-          channel="nabendu82"
-          description="Editing with Kdenlive" 
-          song="Kdenlive in WIndows"
-          likes={155} 
-          shares={120} 
-          messages={55} />
+        {videos.map(({url, channel, description, song, likes, shares, messages}) => 
+          <Video 
+            url={url}
+            channel={channel}
+            description={description} 
+            song={song}
+            likes={likes} 
+            shares={shares} 
+            messages={messages} />
+        )}
       </div>
     </div>
   );
